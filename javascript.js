e15eb5e0e62c2c30fba8515896a22d4f5e79f781
Eikuld <28073955+DawnofSouls@users.noreply.github.com>
2023-03-17 17:39:54 -0700

    Set up the iro.js properties

--- javascript.js
+++ javascript.js
@@ -1,1 +1,43 @@
-let colorPicker = new iro.ColorPicker('#picker');
+let colorPicker = new iro.ColorPicker('#picker', {
+    width: 250 , 
+    borderWidth: 2 ,
+    color: "#ff0099" ,
+    layout: [
+        { 
+          component: iro.ui.Box, 
+          options: {}
+        },
+        { 
+            component: iro.ui.Slider,
+            options: {
+              // can also be 'saturation', 'value', 'red', 'green', 'blue', 'alpha' or 'kelvin'
+              sliderType: 'hue'
+            }
+          },
+          { 
+            component: iro.ui.Slider,
+            options: {
+              // can also be 'saturation', 'value', 'red', 'green', 'blue', 'alpha' or 'kelvin'
+              sliderType: 'saturation'
+            }
+          },
+          { 
+            component: iro.ui.Slider,
+            options: {
+              // can also be 'saturation', 'value', 'red', 'green', 'blue', 'alpha' or 'kelvin'
+              sliderType: 'value'
+            }
+          },
+          { 
+            component: iro.ui.Slider,
+            options: {
+              // can also be 'saturation', 'value', 'red', 'green', 'blue', 'alpha' or 'kelvin'
+              sliderType: 'alpha'
+            }
+          },
+          
+      ]
+    
+});
+
+
